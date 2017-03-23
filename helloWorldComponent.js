33a34fda8b306972456e5104f4d4056652cad75e
CHIGY <gdavid.ptorrez@gmail.com>
2017-03-23 15:32:31 -0400

    firstCommit componente

---
+++ helloWorldComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class helloWorldComponent extends React.Component {
+  render() {
+    return (
+      <div> Hello world!! </div>
+    )
+  }
+}
+export default helloWorldComponent;
